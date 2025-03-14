5846f1930e944af20522f223a5264576e259d34e
hitsuj1 <piotr.pankracy.przybysz@gmail.com>
2025-03-14 02:31:44 +0100

    Update kalkulatorver3.py

--- kalkulatorver2.py
+++ kalkulatorver2.py
@@ -1,25 +1,22 @@
-#Kalkulator Trygonometryczny ver1.
-import math #math library
+import math  # math library #math library
 
-function=str(input("Podaj funkcję: "))
-angle=float(input("Podaj kąt(w stopniach): "))
-lenght=float(input("Podaj długość boku a(b dla cos): "))
+function = input("Podaj funkcję: ")
+angle = float(input("Podaj kąt(w stopniach): "))
+length = float(input("Podaj długość boku a(b dla cos): "))
 
-#input and variables
+# input and variables
 
-if function=="tg":  
-        angle_rad=math.radians(angle)
-        print("tg=", round(math.tan(angle_rad)/lenght)) #tg result
-elif function=="ctg":
-        angle_rad=math.radians(angle)
-        print("ctg=", round(math.tan(angle_rad)*lenght)) #ctg result
-elif function=="sin":
-        angle_rad=math.radians(angle)
-        print("sin=", lenght/math.sin(angle_rad)) #sin result
-elif function=="cos":
-        angle_rad=math.radians(angle)
-        print("cos=", lenght/math.cos(angle_rad)) #cos result
+angle_rad = math.radians(angle) #radian conversion
+
+if function == "tg":
+    print("tg =", round(math.tan(angle_rad) * length, 4))  # tg result
+elif function == "ctg":
+    print("ctg =", round(length / math.tan(angle_rad), 4))  # ctg result
+elif function == "sin":
+    print("sin =", round(length / math.sin(angle_rad), 4))  # sin result
+elif function == "cos":
+    print("cos =", round(length / math.cos(angle_rad), 4))  # cos result
 else:
-        print("Niewłaściwa funkcja") #bad function input
+    print("Niewłaściwa funkcja")  # bad function input
 
-    #działa...ale nie działa xD
+#works like a charm now :) need to add some restrictions about angle and loop it
